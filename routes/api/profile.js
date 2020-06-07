beb5b980831c6d5f2e0ba2d3dfcfa827188386c7
Can <canzhao216@gmail.com>
2020-06-07 02:43:24 -0400

    profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -91,4 +91,81 @@ router.post('/',[ auth, [
           }
 });
 
+
+// @route       GET api/profile
+// @desc        Get all profiles
+// @access      Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route       GET api/profile/user/:user_id
+// @desc        Get profile by user ID
+// @access      Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', 
+        ['name', 'avatar']);
+        if(!profile) return res.status(400).json({ msg: 'Profile not found'});
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind == "ObjectId"){
+            return res.status(400).json({ msg: 'Profile not found'});
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route       DELETE api/profile
+// @desc        Delete profile, user & posts
+// @access      Private
+router.delete('/',auth ,async (req, res) => {
+    try {
+        // @todo - remove user posts
+
+
+        // Remove profile
+        await Profile.findOneAndRemove({
+            user: req.user.id
+        });
+        await User.findOneAndRemove({
+            _id: req.user.id
+        });
+        res.json({ msg: 'User deleted'});
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route       PUT api/profile/experience
+// @desc        Add profile experience
+// @access      Private
+router.put(
+    '/experience', 
+    [
+        auth, 
+        [
+            check('title', 'Title is required')
+            .not()
+            .isEmpty(),
+            check('company', 'Company is required')
+            .not()
+            .isEmpty(),
+            check('from', 'From date is required')
+            .not()
+            .isEmpty(),
+        ]
+    ], async (req, res) => {
+
+});
+
 module.exports = router;
